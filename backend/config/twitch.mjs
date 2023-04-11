d497977a17418ad75dffe36960554b9dc9122ead
Enrique SFranco <enrique.sfranco04@gmail.com>
2023-04-10 19:41:31 -0600

    ruta para enviar el nombre del canal de twitch

--- backend/config/twitch.mjs
+++ backend/config/twitch.mjs
@@ -1,20 +1,20 @@
 import tmi from 'tmi.js'
 
 /* Configuramos el cliente de twitch */
-export function twitch ({ io }) {
+export function twitch ({ io, channel }) {
   const client = new tmi.Client({
     options: { debug: true },
     identity: {
       username: 'toxicity-detection',
-      password: 'oauth:bxbitggxf9q0s45vvkg83a9wte84kv'
+      password: process.env.PASS_TWITCH
     },
-    channels: ['enriquesfranco']
+    channels: [channel] // nombre del canal de twitch
   })
 
   /* Iniciamos el cliente de twitch */
   let messages = []
   client.connect().then(() => {
-    client.on('message', (channel, tags, message, self) => {
+    client.on('message', (tags, message) => {
       const msg = { user: tags['display-name'], message }
       messages = [...messages, msg]
       io.emit('chat message', msg)
