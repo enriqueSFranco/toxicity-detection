89285b9c44994b1efb63cfef766c09fc0b0359db
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-05-22 17:29:00 -0600

    componente Blob

--- frontend/src/components/Blob.jsx
+++ frontend/src/components/Blob.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Blob = () => {
   return (
     <>
-      <div className='absolute top-28 left-1/3 h-96 w-1/2 bg-gradient-to-r from-violet-600 opacity-30 rounded-full blur-3xl' />
-      <div className='absolute top-1/2 left-1/4 h-96 w-1/2 bg-gradient-to-b from-sky-600  opacity-30 rounded-full blur-3xl' />
+      <div className='absolute top-16 left-1/3 h-1/2 w-1/2 bg-gradient-to-r from-violet-600 opacity-50 rounded-full blur-3xl' />
+      <div className='absolute top-1/3 left-1/4 h-1/2 w-1/2 bg-gradient-to-b from-sky-600  opacity-50 rounded-full blur-3xl' />
     </>
   )
 }
