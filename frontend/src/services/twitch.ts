7d25058028c5c8d809c5a4fe29757b1e3c3392c2
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-03-19 11:27:55 -0600

    feat: servicio para usar la api de twitch

---
+++ frontend/src/services/twitch.ts
@@ -0,0 +1,125 @@
+import { API } from '../shared/constants.d'
+import { Token, TwitchChannel } from '../shared/types.d'
+
+const { TWITCH_OAUTH_TOKEN, TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET, ENDPOINT_SEARCH_CHANNEL } = API
+
+function buildStreamURL (channel: string) {
+  const url = new URL(API.URL_STREAMS_TWITCH)
+  url.searchParams.set('user_login', channel)
+  return url
+}
+
+export async function isChannelLive ({ channel }: { channel: string }) {
+  try {
+    const options = {
+      headers: {
+        'Client-ID': "cuozlwcgreix8bpowwg9h6dxdcfhgo",
+        Authorization: `Bearer e10e3nop2r27k111u58zvi55t1uvkf`
+      }
+    }
+    const url = buildStreamURL(channel)
+    const response = await fetch(url, options)
+
+    if (!response.ok) {
+      const error = {
+        err: new Error('Opps, ha ocurrido un error durante la peticion'),
+        status: response.status || '00',
+        statusText: response.statusText || 'Opps, ha ocurrido un error'
+      }
+      throw error
+    }
+    if (response.status === 400) {
+      throw new Error(`El canal ${channel} no existe`)
+    }
+    const json = await response.json()
+    return json
+  } catch (error) {
+  }
+}
+
+export async function getMessagesTwitchChannel (channel) {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ channel })
+    }
+
+    const response = await fetch(`${API.URL_CHANNEL_TWITCH}`, options)
+
+    if (!response.ok) {
+      const error = {
+        err: new Error('Opps, ha ocurrido un error durante la peticion'),
+        status: response.status || '00',
+        statusText: response.statusText || 'Opps, ha ocurrido un error'
+      }
+      throw error
+    }
+    const json = await response.json()
+    return json
+  } catch (error) {
+    if (error.status === 400) {
+      throw new Error(`El canal ${channel} no existe`)
+    } else {
+      throw error
+    }
+  }
+}
+
+
+const getAccessToken = async () => {
+  const options: RequestInit = {
+    method: "POST"
+  }
+  const tokenResponse = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&code=i1xair1gknx7vlyb5ub76ishr4zd4j&grant_type=client_credentials`, options)
+
+  if (!tokenResponse.ok) {
+    const error = {
+      err: new Error('Opps, ha ocurrido un error durante la peticion'),
+      status: tokenResponse.status || '00',
+      statusText: tokenResponse.statusText || 'Opps, ha ocurrido un error'
+    }
+    throw error
+  }
+
+  const tokenJson: Token = await tokenResponse.json()
+
+  return tokenJson.access_token
+}
+
+export const searchChannel = async ({ channelName }: { channelName: string }) => {
+  const token = await getAccessToken()
+  const options: RequestInit = {
+    headers: {
+      "authorization": `Bearer ${token}`,
+      "client-id": `${TWITCH_CLIENT_ID}`,
+    }
+  }
+
+  const channelResponse = await fetch(`https://api.twitch.tv/helix/search/channels?query=${channelName}`, options)
+  console.log("channelResponse", channelResponse)
+
+  const channelJson: TwitchChannel = await channelResponse.json()
+
+  const mappedChannelJson = channelJson.data.map(it => ({
+    broadcasterLanguage: it.broadcaster_language,
+    broadcasterLogin: it.broadcaster_login,
+    displayName: it.display_name,
+    gameId: it.game_id,
+    gameName: it.game_name,
+    id: it.id,
+    isLive: it.is_live,
+    tagIds: it.tag_ids,
+    tags: it.tags,
+    thumbnailURL: it.thumbnail_url,
+    title: it.title,
+    startedAt: it.started_at
+  }))
+
+  return {
+    data: mappedChannelJson,
+    pagination: channelJson.pagination
+  }
+}
