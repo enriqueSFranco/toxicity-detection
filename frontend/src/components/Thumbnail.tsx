629e9b8a287463d9c1c5dd083a30d04aad4a50ea
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-23 10:54:18 -0600

    feat(components): add componentes

--- frontend/src/components/Thumbnail.tsx
+++ frontend/src/components/Thumbnail.tsx
@@ -11,8 +11,8 @@ type Props = {
 const Thumbnail = ({ src, alt, username = 'anonymous', title, tags = [] }: Props) => {
 
   return (
-    <figure className='flex flex-col gap-4'>
-      <img src={src} alt={alt} className='w-full object-cover rounded-2xl overflow-hidden' />
+    <figure className='relative flex flex-col gap-5'>
+      <img src={src} alt={alt} className='w-full object-cover aspect-video rounded-2xl overflow-hidden shadow-twitch-color shadow-md opacity-75' />
       <figcaption className='flex flex-col gap-4'>
         <p className='text-white font-semibold'><span className='font-semibold text-twitch-color'>{username}</span>: {title}</p>
         <Tags data={tags} />
