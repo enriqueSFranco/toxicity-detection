7a2b0d16aaed18f0b80a922bfdaaa1f314253125
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-23 13:09:21 -0600

    chore(services): se eliminaron funciones isChannelLive, getMessagesTwitchChannel y buildStreamURL

--- frontend/src/services/twitch-service.ts
+++ frontend/src/services/twitch-service.ts
@@ -9,65 +9,6 @@ const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET, ENDPOINT_SEARCH_CHANNEL } = API
 //   return url
 // }
 
-// export async function isChannelLive ({ channel }: { channel: string }) {
-//   try {
-//     const options = {
-//       headers: {
-//         'Client-ID': "cuozlwcgreix8bpowwg9h6dxdcfhgo",
-//         Authorization: `Bearer e10e3nop2r27k111u58zvi55t1uvkf`
-//       }
-//     }
-//     const url = buildStreamURL(channel)
-//     const response = await fetch(url, options)
-
-//     if (!response.ok) {
-//       const error = {
-//         err: new Error('Opps, ha ocurrido un error durante la peticion'),
-//         status: response.status || '00',
-//         statusText: response.statusText || 'Opps, ha ocurrido un error'
-//       }
-//       throw error
-//     }
-//     if (response.status === 400) {
-//       throw new Error(`El canal ${channel} no existe`)
-//     }
-//     const json = await response.json()
-//     return json
-//   } catch (error) {
-//   }
-// }
-
-// export async function getMessagesTwitchChannel (channel) {
-//   try {
-//     const options = {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({ channel })
-//     }
-
-//     const response = await fetch(`${API.URL_CHANNEL_TWITCH}`, options)
-
-//     if (!response.ok) {
-//       const error = {
-//         err: new Error('Opps, ha ocurrido un error durante la peticion'),
-//         status: response.status || '00',
-//         statusText: response.statusText || 'Opps, ha ocurrido un error'
-//       }
-//       throw error
-//     }
-//     const json = await response.json()
-//     return json
-//   } catch (error) {
-//     if (error.status === 400) {
-//       throw new Error(`El canal ${channel} no existe`)
-//     } else {
-//       throw error
-//     }
-//   }
-// }
-
 
 const getAccessToken = async (): Promise<Token> => {
   const options: RequestInit = {
